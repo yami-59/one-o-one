6dd8a71c8723e529833a080c789aee4144e289f8
Grimmjow06100 <samy06.fr@gmail.com>
2025-11-11 14:10:00 +0100

    project building

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './components/App.tsx'
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
