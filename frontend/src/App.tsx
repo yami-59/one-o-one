ff8e688687778f33ae2632f578d772a55841fd8a
Grimmjow06100 <samy06.fr@gmail.com>
2025-12-09 01:55:48 +0100

    last commit avant merge entre mon back et le front de celena

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 // /frontend/src/App.tsx
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Exemple from './pages/exemple.tsx';
+// import Exemple from './pages/exemple.tsx';
 import GameRoomWordSearch from './pages/gameRoomWordSearch.tsx';
 // ... autres imports ...
 
@@ -14,7 +14,7 @@ function App() {
             {/* 2. Routes : Définition des chemins */}
             <Routes>
                 {/*La route racine "/" est définie pour afficher HomePage */}
-                <Route path="/" element={<Exemple />} />
+                {/* <Route path="/" element={<Exemple />} /> */}
                 {/* La route est définie avec un paramètre dynamique nommé ':gameId' */}
                 <Route path="/game/wordsearch/:gameId" element={<GameRoomWordSearch />} />
             </Routes>
